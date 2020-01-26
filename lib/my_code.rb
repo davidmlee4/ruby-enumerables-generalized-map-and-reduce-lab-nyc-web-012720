002f94d2dcdf8a56937fc4f4641d95a9c06c7721
Muhan Lee <davidmlee4@gmail.com>
2020-01-26 23:31:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,16 +11,5 @@ end
 
 
 def reduce(source_array, sv=nil)
-  if sv
-    total = sv
-    i = 0
-  else
-    total = source_array[0]
-    i = 1
-  end
-  while i < source_array.length do
-    total = yield(total, source_array[i])
-    i += 1
-  end
-  total
+
 end
